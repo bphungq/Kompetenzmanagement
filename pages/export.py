b56d94242033f00b333514e4248cf6defe43e605
TimRedlin <tim.redlin@tu-dortmund.de>
2025-08-25 10:38:32 +0000

    Export erweitert

--- pages/export.py
+++ pages/export.py
@@ -4,26 +4,34 @@ from functions.menu import default_menu
 from functions.page import footer
 from functions.database import get_dataframe_from_gsheet
 from functions.session_state import check_mode
-from config import GOOGLE_SHEET_ANSWERS, COLUMN_INDEX
+from config import GOOGLE_SHEET_ANSWERS, COLUMN_INDEX, GOOGLE_SHEET_PROFILES, GOOGLE_SHEET_BEDARFE, COLUMN_PROFILE_ID
 
 # -Seitenkonfiguration-
 st.set_page_config(page_title="Export", layout="wide")
 check_mode()
 default_menu()
 
-# -Seiteninhalt-
-st.title("Export")
 
+# Funktion zum Umwandeln in csv
 def convert_for_download(df):
     return df.to_csv(sep=';')
 
-# -Tabelle für Antworten verknüpfen-
-answers = get_dataframe_from_gsheet(GOOGLE_SHEET_ANSWERS, index_col=COLUMN_INDEX, refresh_time_in_minutes=0)
-answers_csv = convert_for_download(answers)
+# Tabelle für Antworten verknüpfen und in csv umwandeln
+data_profiles = get_dataframe_from_gsheet(GOOGLE_SHEET_PROFILES, index_col=COLUMN_PROFILE_ID)
+data_profiles_csv = convert_for_download(data_profiles)
+data_answers = get_dataframe_from_gsheet(GOOGLE_SHEET_ANSWERS, index_col=COLUMN_INDEX)
+data_answers_csv = convert_for_download(data_answers)
+data_bedarfe = get_dataframe_from_gsheet(GOOGLE_SHEET_BEDARFE, index_col=COLUMN_INDEX)
+data_bedarfe_csv = convert_for_download(data_bedarfe)
+
+
+# -Seiteninhalt-
+st.title("Export")
 
 # -Download Buttons-
-# st.download_button(label="Export Profildaten", data=data_profiles_csv, file_name="user_management/profiles.csv")
-st.download_button(label="Export Antworten", data=answers_csv, file_name="antworten.csv")
+st.download_button(label="Export Profile", data=data_profiles_csv, file_name="profile.csv")
+st.download_button(label="Export ausgefüllte Fragebögen", data=data_answers_csv, file_name="antworten.csv")
+st.download_button(label="Export Rollen", data=data_answers_csv, file_name="rollen.csv")
 
 # Fußzeile
 footer()
